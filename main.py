fa0abbbe55a7fea7b1a3b434ae6e5810cc12e116
Laptop <samisamialbizreh@gmail.com>
2024-03-05 08:56:26 +0200

    added venv/ to .gitignore and added token to laptop

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import discord
-import os # default module
+import os  # default module
 from dotenv import load_dotenv
 import datetime
 
-load_dotenv() # load all the variables from the env file
+load_dotenv()  # load all the variables from the env file
 bot = discord.Bot()
+
 
 def log(func):
     def wrapper(*args, **kwargs):
         with open('logs.txt', 'a') as f:
-            f.write('Called function with '+ ''.join([str(arg) for arg in args]) + 'at ' + str(datetime.datetime.now()) + '\n')
+            f.write('Called function with ' + ''.join([str(arg) for arg in args]) + 'at ' + str(
+                datetime.datetime.now()) + '\n')
         val = func(*args, **kwargs)
         return val
+
     return wrapper
 
 
@@ -20,9 +23,11 @@ def log(func):
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
+
 @log
-@bot.slash_command(name = "hello", description = "Say hello to the bot")
+@bot.slash_command(name="hello", description="Say hello to the bot")
 async def hello(ctx):
     await ctx.respond("Hey!")
 
-bot.run(os.getenv('TOKEN')) # run the bot with the token
+
+bot.run(os.getenv('TOKEN'))  # run the bot with the token
